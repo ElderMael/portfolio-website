b587b38ab027e1a69e0a4721d8c28d793f7d3a8f
Miguel Enriquez (menriq2a) <menriq2a@na.xom.com>
2020-01-25 01:23:50 -0600

    Extract Code Rain To Its Own File

---
+++ src/MatrixCodeRain.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const matrix = "ミゲルエンジェルエンリクエスロペス".split("");
+
+export class MatrixCodeRain extends React.Component<{}, {}> {
+
+  private canvas: React.RefObject<HTMLCanvasElement>;
+  private intervalId?: NodeJS.Timeout;
+
+  constructor(props: {}) {
+    super(props);
+    this.canvas = React.createRef();
+  }
+
+  componentDidMount(): void {
+    const canvas = this.canvas.current!;
+    const context = canvas.getContext('2d')!;
+
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    const fontSize = 10;
+    const columns = canvas.width / fontSize;
+
+    const drops: Array<number> = [];
+    for (let x = 0; x < columns; x++)
+      drops[x] = 1;
+
+    this.intervalId = setInterval(() => {
+      context.fillStyle = "rgba(0, 0, 0, 0.04)";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
+      context.fillStyle = "#0F0"; //green text
+      context.font = fontSize + "px arial";
+
+      for (let i = 0; i < drops.length; i++) {
+
+        const text = matrix[Math.floor(Math.random() * matrix.length)];
+        context.fillText(text, i * fontSize, drops[i] * fontSize);
+
+        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
+          drops[i] = 0;
+
+        drops[i]++;
+      }
+    }, 55)
+  }
+
+  componentWillUnmount(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId)
+    }
+  }
+
+  render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+    return (
+        <div>
+          <canvas ref={this.canvas} className="matrix-rain"/>
+        </div>
+    );
+  }
+
+}
