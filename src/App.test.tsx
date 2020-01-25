67801b0637408e57b48aa5cd6133ae679d64a431
Miguel Enriquez (menriq2a) <menriq2a@na.xom.com>
2020-01-24 19:36:37 -0600

    Add Small Test To Check Canvas Is Rendered

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const {baseElement} = render(<App/>);
+  const canvas = baseElement.getElementsByTagName('canvs');
+  expect(canvas.length).toBe(1);
 });
