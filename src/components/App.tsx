51a363ca2506e446b74f5afdf2a4cdae98072761
Miguel Enriquez (menriq2a) <menriq2a@na.xom.com>
2020-01-25 13:58:18 -0600

    Show Components On Page Progresively

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,48 @@ import './App.css';
 import {MatrixCodeRain} from "./MatrixCodeRain";
 import {NameCli} from "./NameCli";
 
-const App: React.FC = () => {
-  return (
-      <div>
-        <MatrixCodeRain/>
-        <NameCli/>
-      </div>
-  );
-};
+class App extends React.Component<{}, { components: any[] }> {
+
+  private interval?: NodeJS.Timeout;
+
+  private componentsToShow: any[] = [
+    <MatrixCodeRain/>,
+    <NameCli/>
+  ];
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      components: []
+    };
+  }
+
+
+  componentDidMount(): void {
+    this.interval = setInterval(() => {
+
+      this.setState({
+        components: [
+          ...this.state.components,
+          this.componentsToShow.shift()
+        ]
+      });
+
+      if (this.componentsToShow.length === 0) {
+        const toClear = this.interval!;
+        clearInterval(toClear);
+      }
+
+    }, 2500);
+  }
+
+  render() {
+    return (
+        <div>
+          {this.state.components}
+        </div>
+    );
+  }
+}
 
 export default App;
