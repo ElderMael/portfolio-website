6a7e48f5e026451af5d2ce51ed080816cd1a6c48
Miguel Enriquez (menriq2a) <menriq2a@na.xom.com>
2020-01-26 16:07:07 -0600

    Finish Navigation Styles

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,24 +1,33 @@
 import React from "react";
+import './Navigation.css'
 
 export const Navigation: React.FC = (props) => {
 
   return (
       <nav className="navigation navbar navbar-dark fixed-top animated fadeIn slow delay-5s">
-        <a className="btn btn-success col-lg-3 col-sm-2" href="/">
-          <span className="fas fa-home" />
-          Home
+        <a className="btn btn-success btn-pill col-lg-2 col-sm-1" href="/">
+          <span className="icon-nav fas fa-2x fa-home" />
+          <span>Home</span>
         </a>
-        <a className="btn btn-success col-lg-3 col-sm-2" href="/code">
-          <span className="fas fa-code" />
-          Code
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/code">
+          <span className="icon-nav fas fa-2x fa-code" />
+          <span>Code</span>
         </a>
-        <a className="btn btn-success col-lg-3 col-sm-2" href="/resume">
-          <span className="fab fa-linkedin" />
-          Resume
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/resume">
+          <span className="icon-nav fab fa-2x fa-linkedin" />
+          <span>Resume</span>
         </a>
-        <a className="btn btn-success col-lg-3 col-sm-2" href="/about">
-          <span className="fas fa-address-card" />
-          About
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/resume">
+          <span className="icon-nav fab fa-2x fa-medium" />
+          <span>Blog</span>
+        </a>
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/resume">
+          <span className="icon-nav fab fa-2x fa-twitter" />
+          <span>Social Media</span>
+        </a>
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/about">
+          <span className="icon-nav fas fa-2x fa-address-card" />
+          <span>About</span>
         </a>
       </nav>
   );
