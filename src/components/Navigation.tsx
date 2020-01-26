a2b866d4cec782f1330986bf73623a8e159b7e09
Miguel Enriquez (menriq2a) <menriq2a@na.xom.com>
2020-01-26 16:40:53 -0600

    Add Links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,27 +6,31 @@ export const Navigation: React.FC = (props) => {
   return (
       <nav className="navigation navbar navbar-dark fixed-top animated fadeIn slow delay-5s">
         <a className="btn btn-success btn-pill col-lg-2 col-sm-1" href="/">
-          <span className="icon-nav fas fa-2x fa-home" />
+          <span className="icon-nav fas fa-2x fa-home"/>
           <span>Home</span>
         </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/code">
-          <span className="icon-nav fas fa-2x fa-code" />
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+           href="https://github.com/ElderMael/">
+          <span className="icon-nav fas fa-2x fa-code"/>
           <span>Code</span>
         </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/resume">
-          <span className="icon-nav fab fa-2x fa-linkedin" />
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+           href="https://www.linkedin.com/in/miguelangelenriquez/">
+          <span className="icon-nav fab fa-2x fa-linkedin"/>
           <span>Resume</span>
         </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/resume">
-          <span className="icon-nav fab fa-2x fa-medium" />
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+           href="https://blog.eldermael.io/">
+          <span className="icon-nav fab fa-2x fa-medium"/>
           <span>Blog</span>
         </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/resume">
-          <span className="icon-nav fab fa-2x fa-twitter" />
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+           href="https://twitter.com/eldermael">
+          <span className="icon-nav fab fa-2x fa-twitter"/>
           <span>Social Media</span>
         </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/about">
-          <span className="icon-nav fas fa-2x fa-address-card" />
+        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/">
+          <span className="icon-nav fas fa-2x fa-address-card"/>
           <span>About</span>
         </a>
       </nav>
