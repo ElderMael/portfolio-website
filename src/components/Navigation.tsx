b964af92c06a056f7c69c2582f357654fe3c8c1e
Miguel Enriquez (menriq2a) <menriq2a@na.xom.com>
2020-01-26 04:41:15 -0600

    Change To Font Awesome

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,19 +5,19 @@ export const Navigation: React.FC = (props) => {
   return (
       <nav className="navigation navbar navbar-dark fixed-top animated fadeIn slow delay-5s">
         <a className="btn btn-success col-lg-3 col-sm-2" href="/">
-          <span className="devicons devicons-chrome" />
+          <span className="fas fa-home" />
           Home
         </a>
         <a className="btn btn-success col-lg-3 col-sm-2" href="/code">
-          <span className="devicons devicons-code" />
+          <span className="fas fa-code" />
           Code
         </a>
         <a className="btn btn-success col-lg-3 col-sm-2" href="/resume">
-          <span className="devicons devicons-linux" />
+          <span className="fab fa-linkedin" />
           Resume
         </a>
         <a className="btn btn-success col-lg-3 col-sm-2" href="/about">
-          <span className="devicons devicons-react" />
+          <span className="fas fa-address-card" />
           About
         </a>
       </nav>
