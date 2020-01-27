d880f99123c10d870c07de380d366d978b0ae378
Miguel Enriquez (menriq2a) <menriq2a@na.xom.com>
2020-01-26 19:02:22 -0600

    Prevent Home And About Links To Navigate Outside The Site

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,39 +1,58 @@
-import React from "react";
+import React, {SyntheticEvent} from "react";
 import './Navigation.css'
 
-export const Navigation: React.FC = (props) => {
+export class Navigation extends React.Component<{}, {}> {
 
-  return (
-      <nav className="navigation navbar navbar-dark fixed-top animated fadeIn slow delay-5s">
-        <a className="btn btn-success btn-pill col-lg-2 col-sm-1" href="/">
-          <span className="icon-nav fas fa-2x fa-home"/>
-          <span>Home</span>
-        </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-           href="https://github.com/ElderMael/">
-          <span className="icon-nav fas fa-2x fa-code"/>
-          <span>Code</span>
-        </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-           href="https://www.linkedin.com/in/miguelangelenriquez/">
-          <span className="icon-nav fab fa-2x fa-linkedin"/>
-          <span>Resume</span>
-        </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-           href="https://blog.eldermael.io/">
-          <span className="icon-nav fab fa-2x fa-medium"/>
-          <span>Blog</span>
-        </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-           href="https://twitter.com/eldermael">
-          <span className="icon-nav fab fa-2x fa-twitter"/>
-          <span>Social Media</span>
-        </a>
-        <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1" href="/">
-          <span className="icon-nav fas fa-2x fa-address-card"/>
-          <span>About</span>
-        </a>
-      </nav>
-  );
+  constructor(props: any) {
+    super(props);
+    this.showAbout = this.showAbout.bind(this);
+    this.showHome = this.showHome.bind(this);
+  }
 
-};
+  showAbout(event: SyntheticEvent): void {
+    event.preventDefault();
+  }
+
+  showHome(event: SyntheticEvent): void {
+    event.preventDefault();
+  }
+
+  render() {
+
+    return (
+        <nav className="navigation navbar navbar-dark fixed-top animated fadeIn slow delay-5s">
+          <a className="btn btn-success btn-pill col-lg-2 col-sm-1"
+             href="/" onClick={this.showHome}>
+            <span className="icon-nav fas fa-2x fa-home"/>
+            <span>Home</span>
+          </a>
+          <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+             href="https://github.com/ElderMael/">
+            <span className="icon-nav fas fa-2x fa-code"/>
+            <span>Code</span>
+          </a>
+          <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+             href="https://www.linkedin.com/in/miguelangelenriquez/">
+            <span className="icon-nav fab fa-2x fa-linkedin"/>
+            <span>Resume</span>
+          </a>
+          <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+             href="https://blog.eldermael.io/">
+            <span className="icon-nav fab fa-2x fa-medium"/>
+            <span>Blog</span>
+          </a>
+          <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+             href="https://twitter.com/eldermael">
+            <span className="icon-nav fab fa-2x fa-twitter"/>
+            <span>Social Media</span>
+          </a>
+          <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+             href="/" onClick={this.showAbout}>
+            <span className="icon-nav fas fa-2x fa-address-card"/>
+            <span>About</span>
+          </a>
+        </nav>
+    );
+
+  }
+}
