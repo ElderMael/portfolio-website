6546a8fd3e375cdc7308107a9f82008f122acaac
ElderMael <sftwr.mael@gmail.com>
2021-09-07 14:29:37 -0500

    Add Some Padding To Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,57 +1,60 @@
 import React, {SyntheticEvent} from "react";
 import './Navigation.css'
+import useWindowDimensions from "../useWindowDimensions";
 
 export class Navigation extends React.Component<{}, {}> {
 
-  constructor(props: any) {
-    super(props);
-    this.showAbout = this.showAbout.bind(this);
-    this.showHome = this.showHome.bind(this);
-  }
+    constructor(props: any) {
+        super(props);
+        this.showAbout = this.showAbout.bind(this);
+        this.showHome = this.showHome.bind(this);
+    }
 
-  showAbout(event: SyntheticEvent): void {
-    event.preventDefault();
-  }
+    showAbout(event: SyntheticEvent): void {
+        event.preventDefault();
+    }
 
-  showHome(event: SyntheticEvent): void {
-    event.preventDefault();
-  }
+    showHome(event: SyntheticEvent): void {
+        event.preventDefault();
+    }
 
-  render() {
+    render() {
+        const {width} = useWindowDimensions();
 
-    return (
-        <nav className="navigation navbar navbar-dark animated fadeIn slow delay-5s">
-          <a href="#home" className="btn btn-success btn-pill col-lg-2 col-sm-1" onClick={this.showHome}>
-            <span className="icon-nav fas fa-2x fa-home"/>
-            <span>Home</span>
-          </a>
-          <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-             target="_blank" rel="noopener noreferrer" href="https://github.com/ElderMael/">
-            <span className="icon-nav fas fa-2x fa-code"/>
-            <span>Code</span>
-          </a>
-          <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-             target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/miguelangelenriquez/">
-            <span className="icon-nav fab fa-2x fa-linkedin"/>
-            <span>Resume</span>
-          </a>
-          <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-             target="_blank" rel="noopener noreferrer" href="https://blog.eldermael.io/">
-            <span className="icon-nav fab fa-2x fa-medium"/>
-            <span>Blog</span>
-          </a>
-          <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-             target="_blank" rel="noopener noreferrer" href="https://twitter.com/eldermael">
-            <span className="icon-nav fab fa-2x fa-twitter"/>
-            <span>Social Media</span>
-          </a>
-            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-               target="_blank" rel="noopener noreferrer" href="https://piratedevradio.tech">
-                <span className="icon-nav fab fa-2x fa-microphone"/>
-                <span>Podcast</span>
-            </a>
-        </nav>
-    );
+        return (
+            <nav
+                className={(width > 3000 ? "container" : "") + "navigation navbar navbar-dark animated fadeIn slow delay-5s"}>
+                <a href="#home" className="btn btn-success btn-pill col-lg-2 col-sm-1" onClick={this.showHome}>
+                    <span className="icon-nav fas fa-2x fa-home"/>
+                    <span>Home</span>
+                </a>
+                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+                   target="_blank" rel="noopener noreferrer" href="https://github.com/ElderMael/">
+                    <span className="icon-nav fas fa-2x fa-code"/>
+                    <span>Code</span>
+                </a>
+                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+                   target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/miguelangelenriquez/">
+                    <span className="icon-nav fab fa-2x fa-linkedin"/>
+                    <span>Resume</span>
+                </a>
+                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+                   target="_blank" rel="noopener noreferrer" href="https://blog.eldermael.io/">
+                    <span className="icon-nav fab fa-2x fa-medium"/>
+                    <span>Blog</span>
+                </a>
+                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+                   target="_blank" rel="noopener noreferrer" href="https://twitter.com/eldermael">
+                    <span className="icon-nav fab fa-2x fa-twitter"/>
+                    <span>Social Media</span>
+                </a>
+                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+                   target="_blank" rel="noopener noreferrer" href="https://piratedevradio.tech">
+                    <span className="icon-nav fab fa-2x fa-microphone"/>
+                    <span>Podcast</span>
+                </a>
+            </nav>
+        );
 
-  }
+    }
 }
