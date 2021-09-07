5c582d103f6f166643cc5336098eab6714b4b58a
ElderMael <sftwr.mael@gmail.com>
2021-09-07 14:40:29 -0500

    Make Buttons Resize

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -19,13 +19,10 @@ export class Navigation extends React.Component<{}, {}> {
     }
 
     render() {
-        const {width} = useWindowDimensions();
-
-        const isWide = width > 3000;
 
         return (
             <nav
-                className={ isWide ? "container" : "navigation navbar navbar-dark animated fadeIn slow delay-5s"}>
+                className="navigation navbar navbar-dark animated fadeIn slow delay-5s">
                 <a href="#home" className="btn btn-success btn-pill col-lg-2 col-sm-1" onClick={this.showHome}>
                     <span className="icon-nav fas fa-2x fa-home"/>
                     <span>Home</span>
