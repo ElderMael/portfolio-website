389a699a4c0f640a9c2173e11746961d68ee1c50
ElderMael <sftwr.mael@gmail.com>
2021-09-07 14:51:03 -0500

    Convert Class To Function

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,61 +2,49 @@ import React, {SyntheticEvent} from "react";
 import './Navigation.css'
 import useWindowDimensions from "../useWindowDimensions";
 
-export class Navigation extends React.Component<{}, {}> {
+export function Navigation(props: any) {
 
-    constructor(props: any) {
-        super(props);
-        this.showAbout = this.showAbout.bind(this);
-        this.showHome = this.showHome.bind(this);
-    }
-
-    showAbout(event: SyntheticEvent): void {
+    function showHome(event: SyntheticEvent): void {
         event.preventDefault();
     }
 
-    showHome(event: SyntheticEvent): void {
-        event.preventDefault();
-    }
+    const {width} = useWindowDimensions();
 
-    render() {
-        const {width} = useWindowDimensions();
+    const isWide = width > 3000;
 
-        const isWide = width > 3000;
+    return (
+        <nav
+            className={isWide ? "container" : " navigation navbar navbar-dark animated fadeIn slow delay-5s"}>
+            <a href="#home" className="btn btn-success btn-pill col-lg-2 col-sm-1" onClick={showHome}>
+                <span className="icon-nav fas fa-2x fa-home"/>
+                <span>Home</span>
+            </a>
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+               target="_blank" rel="noopener noreferrer" href="https://github.com/ElderMael/">
+                <span className="icon-nav fas fa-2x fa-code"/>
+                <span>Code</span>
+            </a>
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+               target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/miguelangelenriquez/">
+                <span className="icon-nav fab fa-2x fa-linkedin"/>
+                <span>Resume</span>
+            </a>
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+               target="_blank" rel="noopener noreferrer" href="https://blog.eldermael.io/">
+                <span className="icon-nav fab fa-2x fa-medium"/>
+                <span>Blog</span>
+            </a>
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+               target="_blank" rel="noopener noreferrer" href="https://twitter.com/eldermael">
+                <span className="icon-nav fab fa-2x fa-twitter"/>
+                <span>Social Media</span>
+            </a>
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+               target="_blank" rel="noopener noreferrer" href="https://piratedevradio.tech">
+                <span className="icon-nav fab fa-2x fa-microphone"/>
+                <span>Podcast</span>
+            </a>
+        </nav>
+    );
 
-        return (
-            <nav
-                className={ isWide ? "container" : " navigation navbar navbar-dark animated fadeIn slow delay-5s"}>
-                <a href="#home" className="btn btn-success btn-pill col-lg-2 col-sm-1" onClick={this.showHome}>
-                    <span className="icon-nav fas fa-2x fa-home"/>
-                    <span>Home</span>
-                </a>
-                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-                   target="_blank" rel="noopener noreferrer" href="https://github.com/ElderMael/">
-                    <span className="icon-nav fas fa-2x fa-code"/>
-                    <span>Code</span>
-                </a>
-                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-                   target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/miguelangelenriquez/">
-                    <span className="icon-nav fab fa-2x fa-linkedin"/>
-                    <span>Resume</span>
-                </a>
-                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-                   target="_blank" rel="noopener noreferrer" href="https://blog.eldermael.io/">
-                    <span className="icon-nav fab fa-2x fa-medium"/>
-                    <span>Blog</span>
-                </a>
-                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-                   target="_blank" rel="noopener noreferrer" href="https://twitter.com/eldermael">
-                    <span className="icon-nav fab fa-2x fa-twitter"/>
-                    <span>Social Media</span>
-                </a>
-                <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
-                   target="_blank" rel="noopener noreferrer" href="https://piratedevradio.tech">
-                    <span className="icon-nav fab fa-2x fa-microphone"/>
-                    <span>Podcast</span>
-                </a>
-            </nav>
-        );
-
-    }
 }
