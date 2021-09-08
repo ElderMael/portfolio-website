a42f3c59331bc3a3986955d6d9f493d024711be5
ElderMael <sftwr.mael@gmail.com>
2021-09-08 12:24:22 -0500

    Updating Column Wide

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,31 +15,31 @@ export function Navigation(props: any) {
     return (
         <nav
             className={ (isWide ? "container" : "" ) + " navigation navbar navbar-dark animated fadeIn slow delay-5s"}>
-            <a href="#home" className="btn btn-success btn-pill col-lg-2 col-sm-1" onClick={showHome}>
+            <a href="#home" className="btn btn-success btn-pill col-lg-2 col-md-1 col-sm-2" onClick={showHome}>
                 <span className="icon-nav fas fa-2x fa-home"/>
                 <span>Home</span>
             </a>
-            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-md-1 col-sm-2"
                target="_blank" rel="noopener noreferrer" href="https://github.com/ElderMael/">
                 <span className="icon-nav fas fa-2x fa-code"/>
                 <span>Code</span>
             </a>
-            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-md-1 col-sm-2"
                target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/miguelangelenriquez/">
                 <span className="icon-nav fab fa-2x fa-linkedin"/>
                 <span>Resume</span>
             </a>
-            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-md-1 col-sm-2"
                target="_blank" rel="noopener noreferrer" href="https://blog.eldermael.io/">
                 <span className="icon-nav fab fa-2x fa-medium"/>
                 <span>Blog</span>
             </a>
-            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-md-1 col-sm-2"
                target="_blank" rel="noopener noreferrer" href="https://twitter.com/eldermael">
                 <span className="icon-nav fab fa-2x fa-twitter"/>
                 <span>Social</span>
             </a>
-            <a className="btn btn-outline-success btn-pill col-lg-2 col-sm-1"
+            <a className="btn btn-outline-success btn-pill col-lg-2 col-md-1 col-sm-2"
                target="_blank" rel="noopener noreferrer" href="https://piratedevradio.tech">
                 <span className="icon-nav fab fas fa-2x fa-microphone"/>
                 <span>Podcast</span>
