ef008f1ccad01faf2e25d6c50eef2eabb7f83242
ElderMael <sftwr.mael@gmail.com>
2021-09-27 01:17:17 -0500

    Use Different Font

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -9,7 +9,7 @@ export function AboutMe() {
             </h3>
             <div className="card-body">
                 <h4 className="card-title">Miguel Enriquez</h4>
-                <div className="card-text">
+                <div className="card-text resume-summary">
                     <p>
                         I am an experienced engineer on the area of Platform Development and Digital Transformations.
                         I have been using my skills on the area of infrastructure and DevOps practices. I help teams
@@ -24,13 +24,13 @@ export function AboutMe() {
                     </p>
                     <p>
                         I am the host of <a href="https://piratedevradio.tech/">PirateDevRadio</a>,
-                        a podcast about software development in general. I am a mentor of
+                        a podcast about software development in general. I am a mentor of &nbsp;
                         <a href="http://twitter.com/devzcommunity">Devz Community</a>.
                     </p>
                 </div>
             </div>
             <div className="card-footer text-muted">
-                <a className="btn btn-outline-success" href="https://blog.eldermael.io/contact/">Contact Me!</a>
+                <a className="contact-me btn btn-outline-success" href="https://blog.eldermael.io/contact/">Contact Me!</a>
             </div>
         </div>
     );
